0f8dbf2986059a2ba93e84f77ce59f742e646c23
Felpor-cmd <filipejosebragadias@gmail.com>
2025-08-19 12:08:16 -0300

    Exemplo_Aula3

---
+++ Exemplo_Aula3/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+
+    public class produto{
+        String nome;
+        String descricao;
+        Double preco;
+        float margemDeLucro;
+    }
+
+    //Metodos
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
