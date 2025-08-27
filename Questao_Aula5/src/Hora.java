5f7d0df384e510057f9dbad59ccc16b2eef4df81
Felpor-cmd <filipejosebragadias@gmail.com>
2025-08-27 10:50:20 -0300

    xulambs_foodsV0.1

---
+++ Questao_Aula5/src/Hora.java
@@ -0,0 +1,14 @@
+public class Hora {
+    int hora;
+    int minutos;
+    
+    public void excedeHora(int hora){}
+
+    public void execedeMinuto(int hora, int minutos){}
+
+    public void imprimeHora(int hora, int minutos){}
+
+    public void soma(int hora, int minutos){}
+
+
+}
